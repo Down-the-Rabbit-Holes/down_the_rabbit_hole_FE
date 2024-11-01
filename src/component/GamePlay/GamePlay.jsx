4fc00e2c49d3420544906038eed56c3b8cde89b8
Stefan Bloom <stefanjbloom88@gmail.com>
2024-11-01 17:06:47 -0600

    feat: mock data made and imported into GamePlay

--- src/component/GamePlay/GamePlay.jsx
+++ src/component/GamePlay/GamePlay.jsx
@@ -1,12 +1,15 @@
 import "./GamePlay.css";
+import animalsData  from "../data/animal_data";
+import animalImages  from "../data/animal_photos";
 
-function GamePlay({animal}) {
+function GamePlay({}) {
+  
 
   return (
     <section className="GamePlay">
       <div className="game-container">
-        <h2 className="game-animal-name">{animal.name}</h2>
-        <img className="game-animal-pic" src={animal.photo_url} alt={`A wild ${animal.name}`} />
+        <h2 className="game-animal-name">{animalsData[0].name}</h2>
+        <img className="game-animal-pic" src={animalImages[0]} alt={`A wild ${animalImages[0].name}`} />
       </div>
     </section>
   );
