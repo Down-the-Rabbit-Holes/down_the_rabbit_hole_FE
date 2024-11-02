ed35f7b0614a8a1ccfbaf47dccfdfedcfaca123e
Marshall Hotaling <marshall.hotaling@gmail.com>
2024-11-01 18:10:03 -0600

    home button implumented

--- src/component/nav_bar/nav_bar.component.jsx
+++ src/component/nav_bar/nav_bar.component.jsx
@@ -1,20 +1,34 @@
 import './nav_bar.css'
+import home from '../../Icons/home.png'
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
 
-import {useEffect} from "react";
+
+
 
 
 const NavBar = () => {
-  useEffect (()=>{
-  },[])
+
+  const navigate = useNavigate()
+  useEffect(() => {
+  }, [])
 
   return (
-    <div className= "navWapper">
-      <nav className = "navBar">
-        <h1 className = "tital">Down the rabbit hole</h1>
+    <div className="navWapper">
+      <nav className="navBar">
+        <h1 className="tital">Down the rabbit hole</h1>
+        <button onClick= {()=> {
+          navigate('/')
+        }}
+        className='home-button'
+        aria-label='Go to Home'
+      >
+        <img className="hiButton"src={home} alt='Home icon'/>
+      </button>
       </nav>
-      
+
     </div>
   );
 };
 
-export default NavBar ;
+export default NavBar;
