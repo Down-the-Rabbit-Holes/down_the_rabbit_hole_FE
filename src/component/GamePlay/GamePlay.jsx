6c028b02e553baee399c377ad777ac9f89ccd2de
Stefan Bloom <stefanjbloom88@gmail.com>
2024-11-01 18:06:40 -0600

    still working on fixing these GamePage methods and rendering properly

--- src/component/GamePlay/GamePlay.jsx
+++ src/component/GamePlay/GamePlay.jsx
@@ -3,35 +3,43 @@ import animalsData  from "../data/animal_data";
 import animalImages  from "../data/animal_photos";
 import NavBar from "../nav_bar/nav_bar.component";
 
-function GamePlay({}) {
-  
+function GamePlay() {
+
   const preyData = animalsData.filter(animal => !animal.characteristics.predators.includes("none", "humans"));
+  const prey = preyData
+  console.log(prey[0])
 
   const getPredators = (prey) => {
     return animalsData.filter(animal => 
-      animal.characteristics.prey.includes(prey.name)
+      animal.characteristics.prey.includes(prey)
     )
   };
+  console.log('getpredators: ', getPredators)
 
   return (
     <section className="GamePlay">
       <NavBar />
-      <div className="game-container">
-        <h2 className="game-animal-name">{preyData[0].name}</h2>
+      <div className="prey-container">
+        <h2 className="prey-animal-name">{prey.name}</h2>
         <img  
-          className="game-animal-pic" 
+          className="prey-animal-pic" 
           src={animalImages[0]} 
           alt={`A wild ${preyData[0].name}`} />
       </div>
       <div className="predator-container">
-        {animalsData.map((animalData, index) => (
-          <span key={index} className="predator-buttons">
-            {animalData[0].characteristics["predators"]}
+        {getPredators(preyData.prey).map((predator, predIndex) => (
+          <span key={predIndex} className="predator-options">
+            {predator.name}
           </span>
         ))}
       </div>
     </section>
   );
 }
+{/* {animalsData.map((animalData, index) => (
+  <span key={index} className="predator-buttons">
+    {animalData[0].characteristics["predators"]}
+  </span>
+))} */}
 
 export default GamePlay;
