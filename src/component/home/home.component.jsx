f31942f424ebf9b3cc565b3d9f26ce4d3d01f620
Candice Cirbo <thecirbos@icloud.com>
2024-11-07 16:44:00 -0700

    Feat: adds tab able to home component

--- src/component/home/home.component.jsx
+++ src/component/home/home.component.jsx
@@ -1,49 +1,61 @@
-import './home.css';
-import NavBar from '../../component/nav_bar/nav_bar.component';
-import { useNavigate } from 'react-router-dom';
+import "./home.css";
+import NavBar from "../../component/nav_bar/nav_bar.component";
+import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-
 
 const Home = () => {
   const [rabbitData, setRabbitData] = useState(null);
   const navigate = useNavigate();
 
-  useEffect(() => {
-  }, []);
+  useEffect(() => {}, []);
 
-
-  const handleGameStart = () => { 
+  const handleGameStart = () => {
     // navigate("/game")
     fetchAnimalData();
-  }
+  };
   function fetchAnimalData() {
-    fetch('http://localhost:3001/api/v1/animals?action_type=start&name=rabbit', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
+    fetch(
+      "http://localhost:3001/api/v1/animals?action_type=start&name=rabbit",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
       }
-    })
-    .then(response => response.json())
-    .then(data => {
-      setRabbitData(data);
-      console.log("rabbitData:", data);
-      navigate("/game", { state: { rabbitData: data } }); 
-    })
-    .catch(error => console.log("Error fetching rabbit data:", error));
-}
-  
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setRabbitData(data);
+        console.log("rabbitData:", data);
+        navigate("/game", { state: { rabbitData: data } });
+      })
+      .catch((error) => console.log("Error fetching rabbit data:", error));
+  }
+
   return (
-    <main className='home-main' data-cy="home-main">
-      <NavBar />
+    <main className="home-main" data-cy="home-main">
+      <NavBar isFavoritesClickable={true} />
       {/* <section className='home-screen'> */}
-        <div>  
-          <img className="game-start-image" data-cy="game-start-image"
-            src="/assets/rabbit.jpg" alt="cute bunny" onClick={handleGameStart}/>
-        </div>
-        <p className='main-page-p' data-cy="home-page-instructions">
-          Click the bunny to discover fascinating facts about them and the intricate food web they belong to!
-        </p>
+      <div>
+        <img
+          className="game-start-image"
+          data-cy="game-start-image"
+          src="/assets/rabbit.jpg"
+          alt="cute bunny"
+          tabIndex="0"
+          onClick={handleGameStart}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" || e.key === " ") {
+              handleGameStart();
+            }
+          }}
+        />
+      </div>
+      <p className="main-page-p" data-cy="home-page-instructions">
+        Click the bunny to discover fascinating facts about them and the
+        intricate food web they belong to!
+      </p>
       {/* </section> */}
     </main>
   );
