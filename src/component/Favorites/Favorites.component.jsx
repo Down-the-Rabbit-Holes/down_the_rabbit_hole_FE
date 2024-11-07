1481bb746d8b6c168f401405b338c13670d18ec1
Candice Cirbo <thecirbos@icloud.com>
2024-11-07 16:38:27 -0700

    Feat: adds tab able feature to favorites

--- src/component/Favorites/Favorites.component.jsx
+++ src/component/Favorites/Favorites.component.jsx
@@ -1,29 +1,36 @@
-import './Favorites.css';
-import NavBar from '../../component/nav_bar/nav_bar.component';
+import "./Favorites.css";
+import NavBar from "../../component/nav_bar/nav_bar.component";
 
 const FavoritesView = ({ favorites }) => {
   console.log("Favorites data:", favorites);
-  
+
   return (
-    <main data-cy="favorite-main" className='favorite-main'>
-      <NavBar />
-      <div className='animal-list' data-cy="animal-list">
-        <h2 data-cy='favorite-header' className='favorite-header'>
-          { favorites.length > 0 ? "Your Bebehs" : "No bebehs"}
+    <main data-cy="favorite-main" className="favorite-main">
+      <NavBar isGamePage={true} isFavoritesClickable={true} />
+      <div className="animal-list" data-cy="animal-list">
+        <h2 data-cy="favorite-header" className="favorite-header">
+          {favorites.length > 0 ? "Your Bebehs" : "No bebehs"}
         </h2>
-        <div className='favorite-list' data-cy='favorite-list'>
+        <div className="favorite-list" data-cy="favorite-list">
           {favorites.map((animal, index) => {
             console.log(`Favorite animal at index ${index}:`, animal);
             return (
-              <div className='favorite-index' data-cy='favorite-index' key={index}>
-                <h3 className='favorite-animal-name' data-cy='favorite-animal-name'>
+              <div
+                className="favorite-index"
+                data-cy="favorite-index"
+                key={index}
+              >
+                <h3
+                  className="favorite-animal-name"
+                  data-cy="favorite-animal-name"
+                >
                   {animal.name}
                 </h3>
-                <img 
+                <img
                   src={animal.photo_url}
-                  alt={`A ${animal.name}`} 
+                  alt={`A ${animal.name}`}
                   className="favorite-animal-pic"
-                  data-cy='favorite-animal-pic'
+                  data-cy="favorite-animal-pic"
                 />
               </div>
             );
