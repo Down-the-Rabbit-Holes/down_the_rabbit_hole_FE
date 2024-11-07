0f83f2862632ead36c34caf9d0b81f1b2f26e57a
Candice Cirbo <thecirbos@icloud.com>
2024-11-07 16:44:34 -0700

    Feat: adds tab able to nav bar

--- src/component/nav_bar/nav_bar.component.jsx
+++ src/component/nav_bar/nav_bar.component.jsx
@@ -1,25 +1,70 @@
-import './nav_bar.css'
+import "./nav_bar.css";
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
-const NavBar = ( {favorites} ) => {
-  const navigate = useNavigate(); 
+const NavBar = ({ favorites, isGamePage, isFavoritesClickable }) => {
+  const navigate = useNavigate();
 
   const handleHomeLoad = () => {
-    navigate("/")
-  }
+    navigate("/");
+  };
 
   const handleFavoritesLoad = () => {
     navigate("/favorites", { state: favorites });
-  }
-  
+  };
+
   return (
     <div className="nav-wrapper">
       <nav className="navBar">
-        <h1 data-cy="title" onClick={handleHomeLoad}>Down The Rabbit Hole</h1>
-        <img data-cy="home-button" className="home-button-icon" src="/assets/home_icon.png" onClick={handleHomeLoad}/>
-        <label data-cy="favorites-button" className="my-save-view" onClick={handleFavoritesLoad}>My Favorites</label>
-        <img data-cy="favorites-button" className="favorites-button-icon" src="/assets/white_heart.png" onClick={handleFavoritesLoad}/>
+        <h1
+          data-cy="title"
+          tabIndex={isGamePage ? "0" : null}
+          onClick={isGamePage ? handleHomeLoad : null}
+          onKeyDown={(e) => {
+            if (isGamePage && (e.key === "Enter" || e.key === " ")) {
+              handleHomeLoad();
+            }
+          }}
+        >
+          Down The Rabbit Hole
+        </h1>
+        <img
+          data-cy="home-button"
+          className="home-button-icon"
+          src="/assets/home_icon.png"
+          onClick={handleHomeLoad}
+          tabIndex="0"
+          onKeyDown={(e) => {
+            if (e.key === "Enter" || e.key === " ") {
+              handleHomeLoad();
+            }
+          }}
+        />
+        <label
+          data-cy="favorites-button"
+          className="my-save-view"
+          onClick={isFavoritesClickable ? handleFavoritesLoad : null}
+          tabIndex={isFavoritesClickable ? "0" : null}
+          onKeyDown={(e) => {
+            if (isFavoritesClickable && (e.key === "Enter" || e.key === " ")) {
+              handleFavoritesLoad();
+            }
+          }}
+        >
+          My Favorites
+        </label>
+        <img
+          data-cy="favorites-button"
+          className="favorites-button-icon"
+          src="/assets/white_heart.png"
+          onClick={isFavoritesClickable ? handleFavoritesLoad : null}
+          tabIndex={isFavoritesClickable ? "0" : null}
+          onKeyDown={(e) => {
+            if (isFavoritesClickable && (e.key === "Enter" || e.key === " ")) {
+              handleFavoritesLoad();
+            }
+          }}
+        />
       </nav>
     </div>
   );
