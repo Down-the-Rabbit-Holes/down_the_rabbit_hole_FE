3c464a8e388b683a57a208420a06cc1cb01538fb
Renee Messersmith <morganrmessersmith@gmail.com>
2024-11-07 19:44:15 -0500

    feat: finally figured this merge mess out

--- src/component/home/home.component.jsx
+++ src/component/home/home.component.jsx
@@ -24,7 +24,7 @@ const Home = () => {
           Accept: "application/json",
         },
       }
-    })
+    )
     .then(response => response.json())
     .then(data => {
       setRabbitData(data);
