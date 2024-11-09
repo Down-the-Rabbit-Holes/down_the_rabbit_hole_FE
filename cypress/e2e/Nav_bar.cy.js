fb5d075bc7c50a82d0c6240ca1c424e283424a27
Marshall Hotaling <marshall.hotaling@gmail.com>
2024-11-08 18:22:37 -0700

    compleated cypress for Nav bar
    Did happy and sad path for Navbar component

--- cypress/e2e/Nav_bar.cy.js
+++ cypress/e2e/Nav_bar.cy.js
@@ -1,22 +1,61 @@
-describe('GamePlay component', () => {
+describe('Nav bar component', () => {
   beforeEach(() =>{
+    cy.intercept(
+      'GET',
+      'https://fathomless-river-45488-66abd37a0e2d.herokuapp.com/api/v1/users/1/user_favorites',
+      { statusCode: 200, fixture: 'favorites' }
+    );
+
+    cy.intercept(
+      'GET',
+      'https://fathomless-river-45488-66abd37a0e2d.herokuapp.com/api/v1/animals?action_type=start&name=rabbit',
+      { statusCode: 200, fixture: 'rabbit' }
+    );
+
+
+
     // hint: you'll want to add an intercept here if you are making a network request on page load!
     cy.visit('http://localhost:3000/')
   });
 
-  it('displays NavBar on page load', () => {
+  it('displays NavBars contents on page load', () => {
     cy.get('[data-cy="title"]').should('have.text', 'Down The Rabbit Hole')
     cy.get('.my-save-view').should('have.text', 'My Favorites')
   })
 
+  it('Should bring you back to home when title is clicked', () => {
+    cy.get('[data-cy="game-start-image"]').click()
+    cy.get('[data-cy="title"]').click()
+    cy.url().should('eq', 'http://localhost:3000/');
+  })
 
+  it('Should load a favorites view when clicked', () => {
+    cy.get('.my-save-view').click()
+    cy.url().should('eq', 'http://localhost:3000/favorites');
+  })
 
+  it('Should load home view when the title is clicked', () => {
+    cy.get('.my-save-view').click()
+    cy.get('[data-cy="title"]').click()
+    cy.url().should('eq', 'http://localhost:3000/');
+  })
+})
 
+describe('Sad path nav bar ', () => {
+  beforeEach(() =>{
+    cy.intercept(
+      'GET',
+      'https://fathomless-river-45488-66abd37a0e2d.herokuapp.com/api/v1/users/1/user_favorites',
+      { statusCode: 500 }
+    );
+    cy.visit('http://localhost:3000/')
+  });
 
+  it('Should load a error massage when my favorites is clicked', () => {
+    cy.get('.my-save-view').click()
+    cy.url().should('eq', 'http://localhost:3000/favorites');
+    cy.get('[data-cy="favorite-header"]').should('have.text', 'No Favorites Yet')
 
-
-
-
-
+  })
 
 })
