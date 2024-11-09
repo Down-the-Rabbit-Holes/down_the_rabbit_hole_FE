9bfc34ea2bcf37379ac659a40ec985eafa880a88
Candice Cirbo <thecirbos@icloud.com>
2024-11-09 10:11:54 -0700

    Fix: fixes gameplay cypress testing, just needed param name in testing

--- cypress/e2e/GamePlay.cy.js
+++ cypress/e2e/GamePlay.cy.js
@@ -1,18 +1,25 @@
 describe('Navigating to the game play view', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/')
-    cy.intercept('GET', 'https://fathomless-river-45488-66abd37a0e2d.herokuapp.com/', {
-      statusCode: 200,
-      body: []
-    })
+    cy.visit('http://localhost:3000/');
+    cy.intercept('GET', 
+      'https://fathomless-river-45488-66abd37a0e2d.herokuapp.com/api/v1/animals?action_type=start&name=rabbit', {
+        statusCode: 200, fixture: 'game-play' 
+        // body: []
+      }).as('rabbit');
+      cy.intercept(
+      'GET',
+      'https://fathomless-river-45488-66abd37a0e2d.herokuapp.com/api/v1/users/1/user_favorites',
+      { statusCode: 200, body: [] }
+    );
   });
 
   it('routes to a different url for the game play and make the API call', () => {
     cy.get('.game-start-image').should('exist').click();
-    cy.url().should('include', '/game');
+    cy.url().should('include', '/game?animal_name=rabbit');
+    cy.wait('@rabbit');
   });
 });
-
+// GET https://fathomless-river-45488-66abd37a0e2d.herokuapp.com/api/v1/users/1/user_favorites
 describe('GamePlay component', () => {
   beforeEach(() => {
     cy.intercept(
@@ -25,7 +32,8 @@ describe('GamePlay component', () => {
       'https://fathomless-river-45488-66abd37a0e2d.herokuapp.com/api/v1/animals?action_type=start&name=rabbit',
       { statusCode: 200, fixture: 'game-play' }
     ).as('rabbit');
-    cy.visit('http://localhost:3000/game');
+    cy.visit('http://localhost:3000/game?animal_name=rabbit');
+    cy.wait('@rabbit');
   });
 
   it('displays rabbit information', () => {
@@ -130,18 +138,18 @@ describe('GamePlay component', () => {
         statusCode: 200,
         body: []
       }
-    );
+    ).as('fetchPredators');
     cy.get('[data-cy="modal-overlay"]').should('not.exist');
     cy.get('[data-cy="eat-me-button"]').click();
     cy.get('[data-cy="modal-overlay"]').should('exist');
     cy.get('[data-cy="predators-header"]').should('exist');
 
-    cy.get('[data-cy="GamePlay-section"]').click();
+    cy.get('[data-cy="modal-overlay"]').click({ force: true });
     cy.get('[data-cy="modal-overlay"]').should('not.exist');
     cy.get('[data-cy="predators-header"]').should('not.exist');
   });
 
-  it('hell', () => {
+  it('checking animal attributes', () => {
     cy.intercept('GET',
       'https://fathomless-river-45488-66abd37a0e2d.herokuapp.com/api/v1/animals?action_type=eat_me&animal_name=rabbit',
       { statusCode: 200,
@@ -170,17 +178,17 @@ describe('GamePlay component', () => {
     cy.get('[data-cy="eat-me-button"]').click();
     cy.wait('@fetchPredators');
 
-    // Click on the predator image (Coyote)
+   
     cy.get('[data-cy="predator-image"]').click();
     cy.wait('@fetchCoyote');
 
-    // Verify that the main page displays the new selected predator (Coyote)
+  
     cy.get('[data-cy="animal-name"]').should('contain', 'COYOTE');
     cy.get('[data-cy="animal-pic"]').should('have.attr', 'src').and('include', 'coyote-standing-on-a-field.jpeg');
     
-    // Check facts about the Coyote
+   
     cy.get('[data-cy="scientific-name-li"]').should('contain', 'Canis latrans');
-    cy.get('[data-cy="animal-pic"]').should('contain', 'https://images.pexels.com/photos/23511068/pexels-photo-23511068/free-photo-of-close-up-of-a-coyote-standing-on-a-field.jpeg?auto=compress&cs=tinysrgb&w=600');
+    cy.get('[data-cy="animal-pic"]').should('have.attr', 'src', 'https://images.pexels.com/photos/23511068/pexels-photo-23511068/free-photo-of-close-up-of-a-coyote-standing-on-a-field.jpeg?auto=compress&cs=tinysrgb&w=600');
     cy.get('[data-cy="diet-li"]').should('contain', "A Coyote's diet includes Rabbit, Mice, Deer");
     cy.get('[data-cy="predators-li"]').should('contain', "A Coyote's predators include Human, Bears, Wolves");
     cy.get('[data-cy="habitat-li"]').should('contain', 'forests, plains and deserts');
