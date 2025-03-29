05a4612484eed710a36edb03d72a6ace587811dc
Renee Messersmith <morganrmessersmith@gmail.com>
2025-03-28 22:33:47 -0400

    Feat: added talking rabbit and text overlay fixed on screen

--- src/component/home/home.component.jsx
+++ src/component/home/home.component.jsx
@@ -1,89 +1,20 @@
 import "./home.css";
 import NavBar from "../../component/nav_bar/nav_bar.component";
 import { Link, Route, Routes } from "react-router-dom";
-// import { useNavigate } from "react-router-dom";
-// import { useEffect, useState } from "react";
 import ForwardIcon from '@mui/icons-material/Forward';
 import ParkSelection from "../ParkSelection/ParkSelection.component";
 
 function Home() {
-  // const navigate = useNavigate();
-  // const [parks, setParks] = useState({data: []});
-
-  // useEffect(() => {
-  //   const fetchParks = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         `https://fathomless-river-45488-66abd37a0e2d.herokuapp.com/api/v1/parks`,
-  //         // "http://localhost:3001/api/v1/parks"
-  //       );
-  //       const data = await response.json();
-  //       setParks(data);
-  //     } catch (error) {
-  //       console.error("Error fetching parks:", error);
-  //     }
-  //   };
-
-  //   fetchParks();
-  // }, []);
-  
-  // const handleParkClick = (parkId, data) => {
-  //   navigate(`/park-details/${parkId}`, {state: data})
-  // };
 
   return (
     <main className="home-main" data-cy="home-main">
       <NavBar isFavoritesClickable={true} />
-      {/* <div className="parks-container">
-        {parks && parks.data.length > 0 ? (
-          parks.data.map((park) => (
-            <div
-              key={park.id}
-              className="park-item"
-              tabIndex="0"
-              onClick={() => {
-                handleParkClick(park.id, park)}}
-                onKeyDown={(e) => {
-                  if (e.key === "Enter" || e.key === " ") {
-                    handleParkClick(park.id, park);
-                  }
-                }}
-            >
-              <img
-                className="parks-poster"
-                data-cy="parks-poster"
-                src={`/assets/parks_posters/${park.attributes.name
-                  .toLowerCase()
-                  .replace(/\s+/g, '_')}.jpeg`}
-                alt={`${park.attributes.name} poster`}
-                
-              />
-            </div>
-          ))
-        ) : (
-          <p>Loading parks...</p>
-        )}
-      </div> */}
-      {/* <p className="main-page-p" data-cy="home-page-instructions">
-        Welcome to Down the Rabbit Hole! Dive into the wonders of nature by selecting a park from our list. Each park page unveils fascinating facts about its unique ecosystem, showcasing the diverse animals that call it home.
-        <br/>
-        <br/>
-        Click a park to start exploring!
-      </p> */}
-      {/* <Routes> */}
+      <div className="rabbit-container">
         <Link to={"/park-selection"} element={<ParkSelection/>}>
-            <ForwardIcon sx={{ fontSize: 150 }} className="arrow"/>
+          <img src="/assets/rabbit-drawing.png" alt="waving rabbit" className="rabbit-image"/>
+          <p className="overlay-text">Welcome! <br></br> Click to journey down <br></br> the rabbit hole!</p>
         </Link>
-      {/* </Routes> */}
-      <div className="rabbit-box">
-        <div className="rabbit-instructions">
-          Click on the arrow to journey down the rabbit hole!
-        </div>
       </div>
-      {/* <div className="rabbit-instructions">
-        Click on the arrow to journey down the rabbit hole!
-      </div>
-      <div className="rabbit-box"></div> */}
     </main>
   );
 };
